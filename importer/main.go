2165fb3835bfed743a80cd8e18519f5afd557b60
Christian Griggs <griggsca91@gmail.com>
2023-05-09 20:13:22 -0400

    fmt

--- importer/main.go
+++ importer/main.go
@@ -11,9 +11,7 @@ import (
 	"github.com/jackc/pgx/v5"
 )
 
-const (
-)
-
+const ()
 
 func main() {
 	data, err := os.ReadFile("../cards.csv")
@@ -28,39 +26,32 @@ func main() {
 	}
 	defer conn.Close(context.Background())
 
-	
 	reader := bytes.NewReader(data)
 
 	csvReader := csv.NewReader(reader)
 	isFirstRow := true
-		headerMap := make(map[string]int)
-
+	headerMap := make(map[string]int)
 
 	for i := 0; i < 10; i++ {
-
-
-
 		record, err := csvReader.Read()
 		if err == io.EOF {
 			break
 		}
 		if err != nil {
 			panic(err)
-		}	
+		}
 		if isFirstRow {
 
 			isFirstRow = false
 
 			// Add mapping: Column/property name --> record index
-			for i,v := range record {
+			for i, v := range record {
 				headerMap[v] = i
 			}
 
 			// Skip next code
 			continue
 		}
-
-
 
 		fmt.Println(record)
 		insertQuery := `insert into
@@ -84,25 +75,24 @@ cards(
 	supertypes
 ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12,$13, $14, $15, $16, $17)`
 
-
-		res, err := conn.Exec(context.Background(), insertQuery, 
-			record[headerMap["id"]], 
-			record[headerMap["artist"]], 
-			record[headerMap["asciiName"]], 
+		res, err := conn.Exec(context.Background(), insertQuery,
+			record[headerMap["id"]],
+			record[headerMap["artist"]],
+			record[headerMap["asciiName"]],
 			record[headerMap["borderColor"]],
-			record[headerMap["defense"]], 
-	record[headerMap["flavorText"]],
-	record[headerMap["life"]],
-	record[headerMap["manaCost"]],
-	record[headerMap["keywords"]],
-	record[headerMap["number"]],
-	record[headerMap["text"]],
-	record[headerMap["power"]],
-	record[headerMap["toughness"]],
-	record[headerMap["type"]],
-	record[headerMap["types"]],
-	record[headerMap["subtypes"]],
-		record[headerMap["supertypes"]],
+			record[headerMap["defense"]],
+			record[headerMap["flavorText"]],
+			record[headerMap["life"]],
+			record[headerMap["manaCost"]],
+			record[headerMap["keywords"]],
+			record[headerMap["number"]],
+			record[headerMap["text"]],
+			record[headerMap["power"]],
+			record[headerMap["toughness"]],
+			record[headerMap["type"]],
+			record[headerMap["types"]],
+			record[headerMap["subtypes"]],
+			record[headerMap["supertypes"]],
 		)
 
 		if err != nil {
